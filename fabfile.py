ea960a5e451dcac11fdb5d1b3814013a38414d0b
chrisamico <eyeseast@gmail.com>
2015-01-18 17:02:07 -0500

    setting up

---
+++ fabfile.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from fabric.api import local, env
--- tarbell_config.py
+++ tarbell_config.py
@@ -8,7 +8,7 @@ Tarbell project configuration
 #SPREADSHEET_KEY = "None"
 
 # Exclude these files from publication
-EXCLUDES = ["*.md", "requirements.txt"]
+EXCLUDES = ['*.md', 'requirements.txt', '*.py']
 
 # Spreadsheet cache lifetime in seconds. (Default: 4)
 # SPREADSHEET_CACHE_TTL = 4
@@ -31,25 +31,11 @@ EXCLUDES = ["*.md", "requirements.txt"]
 S3_BUCKETS = {
     # Provide target -> s3 url pairs, such as:
     #     "mytarget": "mys3url.bucket.url/some/path"
-    # then use tarbell publish mytarget to publish to it
-    
-    "production": "apps.frontline.org/boston",
-    "staging": "frontline-apps/boston",
+    # then use tarbell publish mytarget to publish to it    
 }
 
 # Default template variables
 DEFAULT_CONTEXT = {
-    'data': [   {   'column1': u'row1, column1',
-                    'column2': u'row1, column2'},
-                {   'column1': u'row2, column1',
-                    'column2': u'row2, column2'}],
-    'headline': u'Test headline',
-    'keyed_data': {   'key1': {   'column1': u'key1, column1',
-                                  'column2': u'key1, column2',
-                                  'key': u'key1'},
-                      'key2': {   'column1': u'key2, column1',
-                                  'column2': u'key2, column2',
-                                  'key': u'key2'}},
     'name': 'boston',
     'title': 'Boston Neighborhoods'
 }
